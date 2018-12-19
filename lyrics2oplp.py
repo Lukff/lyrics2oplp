2b79c7bc409c3ed48134f29ce281f1b93b5b47b2
Lucas Fernandes <dlukff@gmail.com>
2018-12-19 13:58:05 -0200

    Add xml output

--- lyrics2oplp.py
+++ lyrics2oplp.py
@@ -1,5 +1,6 @@
 import json
 import re
+from datetime import datetime
 from urllib import request, parse
 from bs4 import BeautifulSoup
 
@@ -76,3 +77,76 @@ def lyrics_from_url(url):
     html = html_from_url(url)
     if source in extractor:
         return extractor[source](html)
+
+def xml_from_lyrics(lyrics, name, author):
+    """Return the lyrics formated as xml."""
+
+    xml_soup = BeautifulSoup('','xml')
+
+    # <song>
+    attributes = {
+        "xmlns": "http://openlyrics.info/namespace/2009/song",
+        "version": "0.8",
+        "createdIn": "lyrics2oplp",
+        "modifiedIn": "",
+        "modifiedDate": datetime.now().isoformat()
+    }
+    song = xml_soup.new_tag("song", attrs = attributes)
+    xml_soup.append(song)
+
+    # <properties>
+    properties = xml_soup.new_tag("properties")
+
+    ## titles
+    titles = xml_soup.new_tag("titles")
+    title = xml_soup.new_tag("title")
+
+    title.append(name) # add song name
+    titles.append(title)
+    properties.append(titles)
+
+    ## authors
+    authors = xml_soup.new_tag("authors")
+    auth = xml_soup.new_tag("author")
+
+    auth.append(author)
+    authors.append(auth)
+    properties.append(authors)
+
+    song.append(properties)
+
+    # <lyrics>
+    lyr = xml_soup.new_tag("lyrics")
+    verse_number = 1
+    verse = xml_soup.new_tag("verse",
+                             attrs={"name": "v{}".format(verse_number)})
+    lyr.append(verse)
+
+    lin_list = lyrics.split('\n')
+    while(lin_list[-1] == ''):
+        lin_list.pop()
+
+    lin = xml_soup.new_tag("lines")
+    for line in lin_list:
+        if line == '':
+            verse.append(lin)
+            verse_number += 1
+            verse = xml_soup.new_tag("verse",
+                                     attrs={"name": "v{}".format(verse_number)})
+            lyr.append(verse)
+            lin = xml_soup.new_tag("lines")
+            continue
+        lin.append(line)
+        lin.append(xml_soup.new_tag("br"))
+
+    verse.append(lin)
+    song.append(lyr)
+
+    return xml_soup
+
+
+if __name__ == '__main__':
+    l = lyrics_from_url('')
+    #print(l.split('\n'))
+    x = xml_from_lyrics(l, "", "")
+    print(x)
