8f898ec21390ff76af8c981b8b426e96dfef6f45
Lucas Fernandes <dlukff@gmail.com>
2018-12-19 16:45:13 -0200

    Add simple user interface

--- lyrics2oplp.py
+++ lyrics2oplp.py
@@ -39,6 +39,24 @@ def html_from_url(url):
     html = response.read()
     return html
 
+def info_from_html(html, name_selector, author_selector):
+    """Return a tuple with song's name and author from a page.
+
+    name_selector -> css selector for song's name
+    author_selector -> css selector for author's name
+    """
+
+    soup = BeautifulSoup(html, 'html.parser')
+    name = soup.select(name_selector)[0]
+    author = soup.select(author_selector)[0]
+
+    return (name.get_text().strip(), author.get_text().strip())
+
+def info_from_letrasmus(html):
+    """Return a tuple with song's name and author from a 'letras.mus.br' page."""
+
+    return info_from_html(html, '.cnt-head_title h1', '.cnt-head_title h2 a')
+
 def lyrics_from_letrasmus(html):
     """Extracts lyrics from the html of a webpage of 'letras.mus.br'."""
 
@@ -50,6 +68,17 @@ def lyrics_from_letrasmus(html):
         lyrics += para.get_text('\n') + '\n\n'
 
     return lyrics
+
+def info_from_vagalume(html):
+    """Return a tuple with song's name and author from a 'vagalume.com.br' page."""
+
+    soup = BeautifulSoup(html, 'html.parser')
+    name = soup.select('#lyricContent h1')[0]
+    author = soup.select('#lyricContent h2 a')[0]
+
+    return info_from_html(html, '#lyricContent h1', '#lyricContent h2 a')
+    return (name.get_text().strip(), author.get_text().strip())
+
 
 def lyrics_from_vagalume(html):
     """Extracts lyrics from a webpage of 'vagalume.com.br'.  """
@@ -67,16 +96,19 @@ def lyrics_from_vagalume(html):
 
 
 def lyrics_from_url(url):
-    """Return the lyrics extracted from a url."""
+    """Return a tuple with song's name, author and lyrics."""
 
     source = identify_url(url)
     extractor = {
-        'letras': lyrics_from_letrasmus,
-        'vagalume': lyrics_from_vagalume
+        'letras': (lyrics_from_letrasmus, info_from_letrasmus),
+        'vagalume': (lyrics_from_vagalume, info_from_vagalume)
     }
     html = html_from_url(url)
     if source in extractor:
-        return extractor[source](html)
+        info = extractor[source][1](html)
+        return (info[0], info[1], extractor[source][0](html))
+    raise Exception("Unknow url's source.")
+
 
 def xml_from_lyrics(lyrics, name, author):
     """Return the lyrics formated as xml."""
@@ -144,9 +176,20 @@ def xml_from_lyrics(lyrics, name, author):
 
     return xml_soup
 
+def xml_from_url(url):
+    """Return a OpenLyrics' XML from a url."""
+
+    data = lyrics_from_url(url)
+    return xml_from_lyrics(data[2], data[0], data[1])
+
+def save_to_file(filename, xml_data):
+    """Write data to a file."""
+
+    with open('{}.xml'.format(filename), 'w') as file:
+        file.write(str(xml_data))
+
 
 if __name__ == '__main__':
-    l = lyrics_from_url('')
-    #print(l.split('\n'))
-    x = xml_from_lyrics(l, "", "")
-    print(x)
+    xml = xml_from_url(input('url: '))
+    arquivo = input('salvar como: ')
+    save_to_file(arquivo, xml)
